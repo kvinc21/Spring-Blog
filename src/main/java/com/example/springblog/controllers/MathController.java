e76084c5381b062e2540a5befe0a320c6c7e1a68
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-09-27 13:11:36 -0500

    upstream test

--- src/main/java/com/example/springblog/controllers/MathController.java
+++ src/main/java/com/example/springblog/controllers/MathController.java
@@ -7,7 +7,7 @@ public class MathController {
 
     @GetMapping("/add/3/add/4")
     @ResponseBody
-    public int mathResponse(){
+    public int mathResponse() {
         int num1 = 3;
         int num2 = 4;
         return num1 + num2;
